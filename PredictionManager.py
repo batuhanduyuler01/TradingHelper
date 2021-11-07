e07867e90ddefa6bd24a8323678aa77bd75b9997
batuhanduyuler01 <duyuler15@itu.edu.tr>
2021-11-08 01:00:36 +0300

    input change applied.

--- PredictionManager.py
+++ PredictionManager.py
@@ -23,14 +23,29 @@ class PredictionManager:
         self.algoManager.startRSI()
 
     def printTradings(self, rowNumber = 30):
-        self.indicatorManager.printCommonDataFramewithClose(rowNumber)
+        #self.indicatorManager.printCommonDataFramewithClose(rowNumber)
         self.indicatorManager.printOnlyTradings(rowNumber)
+        self.indicatorManager.printCommonDataFramewithClose(1)
 
-
-predictionManager = PredictionManager('KARSN.IS', '1y', '1d')
-#predictionManager.printTradings()
-print('Oynanan Market: KARSN.IS')
+userInput = input("Market İsmini Girin: ")
+#userInput = 'CANTE.IS'
+predictionManager = PredictionManager(userInput.upper(), '5d', '1m')
+print(f'Oynanan Market: {userInput}')
 predictionManager.startTrading()
 rsi_backTest = predictionManager.algoManager.getOnlyRsiData()
 myBackTest = backtest.BackTesting(rsi_backTest, 1000)
 myBackTest.implementBackTest()
+
+print("\n\n\n")
+predictionManager.printTradings()
+
+# use "period" instead of start/end
+        # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
+        # (optional, default is '1mo')
+        #period = "ytd",
+
+# fetch data by interval (including intraday if period < 60 days)
+        # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
+        # (optional, default is '1d')
+        #interval = "1m",
+
